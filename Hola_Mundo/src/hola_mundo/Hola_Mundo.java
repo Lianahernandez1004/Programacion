adf699e3c5e86845384cfa3b22c4f9d20000f38d
Lianahernandez1004 <liana_hernandez@unitec.edu>
2026-07-25 05:04:42 +0200

    Hola

--- Hola_Mundo/src/hola_mundo/Hola_Mundo.java
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -15,6 +15,44 @@ public class Hola_Mundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+   System.out.println("Hola.. me llamo liana");
+   System.out.println("Este sera mi 1er");
+   System.out.println("PROGRAMAAA!!!");
+        
+        System.out.print("Vamos camino a");
+        System.out.print("la clase de");
+        System.out.print("Progra!!");
+        
+        System.out.printf("Tu apellido es %S\n","Hernandez");
+        //System.out.println("");
+        
+        System.out.printf("%S Tu apellido es\n", "Hernandez");
+        //System.out.println("");
+        
+       System.out.printf("Tu apellido %S es\n", "Hernandez");
+       System.out.println("Hola\n este es\n un nuevo \n amanecer");
+       System.out.println("Hola\t este es \t un nuevo \t amanecer");
+       
+       // Variables
+       int edad; // Tipo Entero
+       double peso; // Tipo Decimal
+       char inicial; // Tipo Caracter -- Letras, digitos, simbolos Espaciales
+      String nombre; // Tipo Cadena- tipo de dato complejo
+        boolean estado; // Tipo estado --- true(verdadero) / False (falso)
+        
+        edad=21;
+        peso=60.5;
+        inicial='D'; // alt+39 -- '
+        nombre="Liana"; 
+        
+        System.out.printf("Tengo %d años\n",edad);
+        System.out.printf("Mi peso ideal es %.2f \n",peso);
+        System.out.printf("Dime %c por mi inicial",inicial);
+        System.out.printf("%s es mi nombre", nombre);
+  
+    }// Fin de Main
 }
+          
+
+    
+   
